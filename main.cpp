f2b4488089832ef7445c6bd1824f3ec0f7c4bf4d
mMichele-github <100371043+mMichele-github@users.noreply.github.com>
2022-04-02 11:17:23 +0200

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,6 +1,27 @@
 #include <iostream>
 using namespace std;
-
-int main() {
-   return 0;
+int main()
+{
+    int n=0;
+    cout << "Inserire un numero intero positivo maggiore o uguale di 2: " << endl;
+    cin >> n;
+    int t1=1, t2=1, c=2;
+    int next=0;
+    if ((n>=2))
+    {
+        cout << t1 << endl <<t2 << endl;
+        while ((n>c))
+        {
+            cout << (next=t1+t2) << endl;
+            c++;
+            t1=t2;
+            t2=next;
+        }
+    }
+    else
+    {
+        cout << "errore" << endl;
+    }
+    return 0;
 }
+
